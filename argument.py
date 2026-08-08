6e42b8bb6144b79440602692d644231ff67c7527
maruti-21 <marutipatil0342@gmail.com>
2026-08-08 22:14:08 +0530

    Add files via upload
    its r 3rd day

---
+++ argument.py
@@ -0,0 +1,28 @@
+# # how many types of arguments can we pass in function 
+# 1. positinal argument
+# 2.keyword argument
+# 3.  default argument
+# 4. variable number of argument/ variable length argument
+
+
+# positional argument
+def login(maruti,patil):
+    if username==maruti:
+        print("login successful")
+    elif:
+        password==patil
+        PendingDeprecationWarning
+    else:
+        print("error")
+username=input("enter user name:")
+password=input("enter your password")
+login(username,password)
+
+def cityname(city="goa"):
+    print(city)
+    
+    
+    
+cityname("delhi")
+cityname("nagpur")
+cityname()
